62cba3804c4f0acdf187cf562611412423c839f7
RoeiWagner1 <roeiwagner1@gmail.com>
2023-11-04 16:21:48 +0200

    getfavorites + delete

--- main.py
+++ main.py
@@ -93,6 +93,35 @@ async def view_wishlist(update: Update, context: CallbackContext):
         await context.bot.send_message(chat_id, "Your wish list is empty.")
 
 
+async def view_favorites(update: Update, context: CallbackContext):
+    chat_id = update.message.chat_id
+
+    # Fetch all restaurants from the wish list for the current chat_id
+    cursor.execute('SELECT * FROM visited_restaurants WHERE chat_id = ?', (chat_id,))
+    result = cursor.fetchall()
+
+    if result:
+        response_message = "<u>Your favorites:</u>\n\n"
+        for row in result:
+            restaurant_name = row[1]  # Assuming name is in the second column
+            address = row[2]  # Assuming address is in the third column
+            specific_dishes = row[4]  # Assuming comments are in the fifth column
+
+            response_message += f"<b>שם:</b> {restaurant_name}\n" \
+                               f"<b>כתובת:</b> {address}\n" \
+                               f"<b>מנות אהובות:</b> {specific_dishes}\n\n"
+
+        await context.bot.send_message(chat_id, response_message, parse_mode=ParseMode.HTML)
+    else:
+        await context.bot.send_message(chat_id, "Your wish list is empty.")
+
+
+async def delete_restaurant(update: Update, context: CallbackContext):
+    chat_id = update.message.chat_id
+    await context.bot.send_message(chat_id, "Which restaurant do you want to delete? Enter the name of the restaurant:")
+    context.user_data['pending_action'] = 'delete_restaurant'
+
+
 async def handle_user_input(update: Update, context: CallbackContext):
     chat_id = update.message.chat_id
     user_input = update.message.text
@@ -101,9 +130,22 @@ async def handle_user_input(update: Update, context: CallbackContext):
         pending_action = context.user_data['pending_action']
 
         if pending_action == 'restaurant_name_visited':
-            context.user_data['restaurant_name'] = user_input
-            await context.bot.send_message(chat_id, "Enter the address of the restaurant:")
-            context.user_data['pending_action'] = 'restaurant_address_visited'
+            cursor.execute('SELECT * FROM visited_restaurants WHERE chat_id = ? AND name = ?',
+                           (chat_id, user_input))
+            result_visited = cursor.fetchone()
+
+            # Check if the restaurant already exists in the wish list
+            cursor.execute('SELECT * FROM wish_list WHERE chat_id = ? AND name = ?',
+                           (chat_id, user_input))
+            result_wishlist = cursor.fetchone()
+
+            if result_visited or result_wishlist:
+                await context.bot.send_message(chat_id, "This restaurant appears on one of your lists.")
+                # Decide what to do next; possibly return or take some other action
+            else:
+                context.user_data['restaurant_name'] = user_input
+                await context.bot.send_message(chat_id, "Enter the address of the restaurant:")
+                context.user_data['pending_action'] = 'restaurant_address_visited'
 
         elif pending_action == 'restaurant_address_visited':
             context.user_data['restaurant_address'] = user_input
@@ -165,9 +207,22 @@ async def handle_user_input(update: Update, context: CallbackContext):
             context.user_data.clear()
 
         elif pending_action == 'restaurant_name_wishlist':
-            context.user_data['restaurant_name'] = user_input
-            await context.bot.send_message(chat_id, "Enter the address of the restaurant:")
-            context.user_data['pending_action'] = 'restaurant_address_wishlist'
+            cursor.execute('SELECT * FROM visited_restaurants WHERE chat_id = ? AND name = ?',
+                           (chat_id, user_input))
+            result_visited = cursor.fetchone()
+
+            # Check if the restaurant already exists in the wish list
+            cursor.execute('SELECT * FROM wish_list WHERE chat_id = ? AND name = ?',
+                           (chat_id, user_input))
+            result_wishlist = cursor.fetchone()
+
+            if result_visited or result_wishlist:
+                await context.bot.send_message(chat_id, "This restaurant appears on one of your lists.")
+                # Decide what to do next; possibly return or take some other action
+            else:
+                context.user_data['restaurant_name'] = user_input
+                await context.bot.send_message(chat_id, "Enter the address of the restaurant:")
+                context.user_data['pending_action'] = 'restaurant_address_wishlist'
 
         elif pending_action == 'restaurant_address_wishlist':
             context.user_data['restaurant_address'] = user_input
@@ -193,6 +248,29 @@ async def handle_user_input(update: Update, context: CallbackContext):
                                               "You can use /getwishlist to view your wish list.")
             context.user_data.clear()  # Clear user data after processing
 
+        elif pending_action == 'delete_restaurant':
+            user_input = update.message.text
+            chat_id = update.message.chat_id
+
+            # Check which table to delete from (visited_restaurants or wish_list)
+            cursor.execute('SELECT * FROM visited_restaurants WHERE chat_id = ? AND name = ?', (chat_id, user_input))
+            result_visited = cursor.fetchone()
+
+            cursor.execute('SELECT * FROM wish_list WHERE chat_id = ? AND name = ?', (chat_id, user_input))
+            result_wishlist = cursor.fetchone()
+
+            # Deleting the restaurant if found
+            if result_visited:
+                cursor.execute('DELETE FROM visited_restaurants WHERE chat_id = ? AND name = ?', (chat_id, user_input))
+                conn.commit()
+                await context.bot.send_message(chat_id, f"The restaurant has been deleted from your favorites list.")
+            elif result_wishlist:
+                cursor.execute('DELETE FROM wish_list WHERE chat_id = ? AND name = ?', (chat_id, user_input))
+                conn.commit()
+                await context.bot.send_message(chat_id, f"The restaurant has been deleted from your wish-list.")
+            else:
+                await context.bot.send_message(chat_id, "The restaurant you want to delete was not found.")
+
     else:
         if 'hello' in user_input.lower() or 'hey' in user_input.lower() or 'שלום' in user_input:
             await context.bot.send_message(chat_id, "Hello! \nNice to have you here :)")
@@ -203,12 +281,6 @@ async def handle_user_input(update: Update, context: CallbackContext):
         else:
             await context.bot.send_message(chat_id, "Sorry, I didn't understand what you wrote :( \nI'm a bot so I respond to specific words or built-in commands that you can see by typing the character /")
             return
-
-
-# async def view_wishlist(update: Update, context: CallbackContext):
-
-
-# Responses
 
 
 async def error(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -225,9 +297,9 @@ if __name__ == '__main__':
     app.add_handler(CommandHandler('getinfo', get_restaurant_info))
     app.add_handler(CommandHandler("addtowishlist", add_to_wishlist))
     app.add_handler(CommandHandler("getwishlist", view_wishlist))
+    app.add_handler(CommandHandler("getfavorites", view_favorites))
+    app.add_handler(CommandHandler("delete", delete_restaurant))
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_user_input))
-
-    # Messages
 
     # Error
     app.add_error_handler(error)
